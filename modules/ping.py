a5ebdd6058d8bba168f408345c1998de6c8eb2c7
rany <ranygh@riseup.net>
2021-11-05 17:52:09 +0200

    Add ping module

---
+++ modules/ping.py
@@ -0,0 +1,44 @@
+"""
+ping module for botty
+"""
+
+import subprocess
+
+
+def ping(nick, source, privmsg, netmask, is_channel, send_message):
+    """
+    ping does a ping to the address provided
+    """
+    ret = None
+    if privmsg.startswith(".ping "):
+        ret = True
+        cmd = privmsg[len(".ping ") :]
+        cmd = cmd.split(" ")[0]
+        if cmd.startswith("-"):
+            return ret
+
+        process = subprocess.run(
+            ["ping", "-c", "4", "-i", "0.2", cmd],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True,
+        )
+
+        if process.returncode == 0:
+            for line in process.stdout.split("\n"):
+                if line.strip() != "":
+                    msg = line.strip()
+        else:
+            msg = process.stderr.strip()
+            if msg == "":
+                for line in process.stdout.split("\n"):
+                    if line.strip() != "":
+                        msg = line.strip()
+
+        if msg.strip() != "":
+            if is_channel:
+                send_message(f"{nick}, {msg}", source)
+            else:
+                send_message(f"{msg}", source)
+
+    return ret
