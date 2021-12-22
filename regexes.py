2140aac843a2e51b1121a64e1aa05ecf836ad466
rany <ranygh@riseup.net>
2021-12-22 22:10:20 +0200

    Needs explicit https?

--- regexes.py
+++ regexes.py
@@ -8,6 +8,7 @@ ircspecial = re.compile(
     r"\x1f|\x01|\x02|\x12|\x0f|\x1d|\x16|\x0f(?:\d{1,2}(?:,\d{1,2})?)?|\x03(?:\d{1,2}(?:,\d{1,2})?)?",
     re.UNICODE,
 )
-urlregex = re.compile(r"((https?://|[\S]*\.[\S]|\[[^\]]*\])([\S]*))")
+#urlregex = re.compile(r"((https?://|[\S]*\.[\S]|\[[^\]]*\])([\S]*))")
+urlregex = re.compile(r"((https?://[\S]+))")
 ytregex = re.compile(r"(\.|^)(youtube\.com|youtu\.be)$")
 twregex = re.compile(r"(\.|^)twitter\.com$")
